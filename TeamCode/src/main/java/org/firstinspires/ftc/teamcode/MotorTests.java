ca5313763a985d32f65820bd2c46f2b25684ea50
shark-cyber <kancharlashourya@gmail.com>
2023-11-03 19:30:14 -0500

    Save changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTests.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTests.java
@@ -1,30 +1,59 @@
 package org.firstinspires.ftc.teamcode;
 
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+
 @TeleOp
 public class MotorTests extends LinearOpMode {
-    private DcMotor testmotor = null;
+    private DcMotor frontLeft = null;
+    private DcMotor frontRight = null;
+    private DcMotor backLeft = null;
+    private DcMotor backRight = null;
+    @Override
 
 
     public void runOpMode() throws InterruptedException{
-        testmotor = hardwareMap.get(DcMotor.class, "testmotor");
-
+        frontLeft = hardwareMap.get(DcMotor.class, "frontleft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontright");
+        backLeft = hardwareMap.get(DcMotor.class, "backleft");
+        backRight = hardwareMap.get(DcMotor.class, "backright");
         waitForStart();
         if (isStopRequested())
             return;
         while (opModeIsActive()) {
             if (gamepad1.b) {
-                testmotor.setDirection(DcMotorSimple.Direction.FORWARD);
-
-                testmotor.setPower(gamepad1.left_stick_y);
+                frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+                frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+                backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+                backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+                frontLeft.setPower(gamepad1.left_stick_y);
+                frontRight.setPower(gamepad1.left_stick_y);
+                backLeft.setPower(gamepad1.left_stick_y);
+                backRight.setPower(gamepad1.left_stick_y);
             }
+           /*if (gamepad1.a) {
+               frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+               frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+               backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+               backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+               frontLeft.setPower(gamepad1.right_stick_x);
+               frontRight.setPower(gamepad1.right_stick_x);
+               backLeft.setPower(gamepad1.right_stick_x);
+               backRight.setPower(gamepad1.right_stick_x);
+
+
+               }*/
         }
 
 
 
-        }
+
+
+
     }
+}
+
