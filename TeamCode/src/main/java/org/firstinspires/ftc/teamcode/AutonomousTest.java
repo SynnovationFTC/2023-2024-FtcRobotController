396ac32913e4a6f7c2018de56dc9d19f82bcbe85
SynnovationFTC <145935156+SynnovationFTC@users.noreply.github.com>
2023-11-29 20:56:36 -0600

    Create AutonomousTest.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTest.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous
+public class AutonomousTest extends LinearOpMode {
+    public void runOpMode() {
+        waitForStart();
+        if (opModeIsActive()) {
+            while (opModeIsActive()) {
+            }
+        }
+    }
+
+}
