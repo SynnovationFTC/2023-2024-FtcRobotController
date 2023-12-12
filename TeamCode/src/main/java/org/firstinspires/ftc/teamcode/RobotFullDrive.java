eb4dd86aa9c2fbcde0ba3e72a108e5a0172e2502
SynnovationFTC <145935156+SynnovationFTC@users.noreply.github.com>
2023-12-11 19:21:20 -0600

    Update Gradle and FullDrive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotFullDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotFullDrive.java
@@ -149,8 +149,8 @@ public class RobotFullDrive extends LinearOpMode {
             }
 
             // drive using manual POV Joystick mode.  Slow things down to make the robot more controllable.
-            drive = -gamepad1.left_stick_y / 2.0;  // Reduce drive rate to 50%.
-            strafe = -gamepad1.left_stick_x / 2.0;  // Reduce strafe rate to 50%.
+            drive = -gamepad1.left_stick_y * 0.75;  // Reduce drive rate to 50%.
+            strafe = -gamepad1.left_stick_x * 0.75;  // Reduce strafe rate to 50%.
             //turn = -gamepad1.right_stick_x / 3.0
             turn = gamepad1.right_stick_x / 3.0;  // Reduce turn rate to 33%.
             telemetry.addData("Details", "Drive %5.2f, Strafe %5.2f, Turn %5.2f ", drive, strafe, turn);
