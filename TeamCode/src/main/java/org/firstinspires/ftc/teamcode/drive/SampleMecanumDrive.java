41ffdabc5b0c415429ff5bafdba307beb4480896
SynnovationFTC <145935156+SynnovationFTC@users.noreply.github.com>
2024-01-07 16:31:37 -0600

    Fix Roadrunner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
@@ -33,6 +33,7 @@ import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
@@ -94,10 +95,12 @@ public class SampleMecanumDrive extends MecanumDrive {
         }
 
 
-        leftFront = hardwareMap.get(DcMotorEx.class, "frontleft");
+        leftFront = hardwareMap.get(DcMotorEx.class, "frontright");
         leftRear = hardwareMap.get(DcMotorEx.class, "backleft");
-        rightRear = hardwareMap.get(DcMotorEx.class, "backright");
-        rightFront = hardwareMap.get(DcMotorEx.class, "frontright");
+        rightRear = hardwareMap.get(DcMotorEx.class, "frontleft");
+        rightFront = hardwareMap.get(DcMotorEx.class, "backright");
+        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightRear.setDirection(DcMotorSimple.Direction.REVERSE);
 
         motors = Arrays.asList(leftFront, leftRear, rightRear, rightFront);
 
