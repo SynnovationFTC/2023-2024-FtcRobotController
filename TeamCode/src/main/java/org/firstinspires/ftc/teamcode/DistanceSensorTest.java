62646d7af00d6c7cf736561e56b1ba112a29f223
shark-cyber <kancharlashourya@gmail.com>
2024-01-07 16:39:28 -0600

    Distance Test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DistanceSensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DistanceSensorTest.java
@@ -1,10 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
@@ -31,13 +29,11 @@ public class DistanceSensorTest extends LinearOpMode{
         while(opModeIsActive()){
             telemetry.addData("Status", "Running");
         }
-        double leftdistance = left.getDistance(DistanceUnit.CM);
+
         telemetry.addData("deviceName",left.getDeviceName() );
         telemetry.addData("left", String.format("%.01f cm", left.getDistance(DistanceUnit.CM)));
-        double middledistance = middle.getDistance(DistanceUnit.CM);
         telemetry.addData("deviceName",middle.getDeviceName() );
         telemetry.addData("middle", String.format("%.01f cm", middle.getDistance(DistanceUnit.CM)));
-        double rightdistance = right.getDistance(DistanceUnit.CM);
         telemetry.addData("deviceName",right.getDeviceName() );
         telemetry.addData("right", String.format("%.01f cm", right.getDistance(DistanceUnit.CM)));
 
