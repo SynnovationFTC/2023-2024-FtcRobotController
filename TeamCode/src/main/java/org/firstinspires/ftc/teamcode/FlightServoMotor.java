c91f17280265acabdb93ea89ae1f88853ec7f145
shark-cyber <kancharlashourya@gmail.com>
2024-01-08 18:51:45 -0600

    Distance Test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FlightServoMotor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FlightServoMotor.java
@@ -8,21 +8,44 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp
 
 public class FlightServoMotor extends LinearOpMode {
+    Servo top = hardwareMap.get(Servo.class, "topservo");
+    Servo down = hardwareMap.get(Servo.class, "bottomservo");
+    double topservoposition = top.getPosition();
+    double bottomservoposition = down.getPosition();
+    private static double topservoincrement = 0.01;
+    private static double bottomservoincrement = 0.01;
 
 
     @Override
     public void runOpMode() {
         Servo top = hardwareMap.get(Servo.class, "topservo");
         Servo down = hardwareMap.get(Servo.class, "bottomservo");
-        // Wait for the game to start (driver presses PLAY)
+        top.getPosition();
         waitForStart();
         while (opModeIsActive()) {
             if (gamepad1.dpad_up){
-                top.setPosition(0.5);
+                topservoposition -= topservoincrement;
+                top.setPosition(topservoposition);
+                telemetry.addData("Dpad", "signal worksup!");
             }
             if (gamepad1.dpad_down){
-                down.setPosition(1);
+                topservoposition += topservoincrement;
+                top.setPosition(topservoposition);
+                telemetry.addData("Dpad", "signal worksdown!");
             }
+            if (gamepad1.dpad_left){
+                bottomservoposition -= bottomservoincrement;
+                top.setPosition(bottomservoposition);
+
+                telemetry.addData("Dpad", "signal worksleft!");
+            }
+
+            if (gamepad1.dpad_right){
+                bottomservoposition += bottomservoincrement;
+                top.setPosition(bottomservoposition);
+                telemetry.addData("Dpad", "signal worksright!");
+            }
+            telemetry.update();
         }
     }
 }
