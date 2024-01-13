f13db17a8c7db9ea4d2ec45d50a6fa86e1cf17b5
SynnovationFTC <145935156+SynnovationFTC@users.noreply.github.com>
2024-01-13 15:48:43 -0600

    Update RRSplineTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RRSplineTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RRSplineTest.java
@@ -22,8 +22,8 @@ public class RRSplineTest extends LinearOpMode {
                 .splineTo(new Vector2d(11.89, -33.37), Math.toRadians(91.82))
                 .lineTo(new Vector2d(23.77, -55.31))
                 .splineTo(new Vector2d(34.85, -51.23), Math.toRadians(25.84))
-                .splineTo(new Vector2d(39.09, -38.40), Math.toRadians(39.14))
-                .splineTo(new Vector2d(48.92, -36.00), Math.toRadians(0.00))
+                .splineTo(new Vector2d(36.57, -37.49), Math.toRadians(39.14))
+                .splineTo(new Vector2d(50.06, -36.57), Math.toRadians(0.00))
                 .build();
         drive.setPoseEstimate(traj.start());
         /*
