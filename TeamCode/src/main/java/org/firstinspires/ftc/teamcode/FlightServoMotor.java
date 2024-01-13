262916edee9440b0cbe5d55d708bb6013f348480
shark-cyber <kancharlashourya@gmail.com>
2024-01-12 19:40:29 -0600

    Distance Test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FlightServoMotor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FlightServoMotor.java
@@ -4,48 +4,52 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-
 @TeleOp
-
 public class FlightServoMotor extends LinearOpMode {
-    Servo top = hardwareMap.get(Servo.class, "topservo");
-    Servo down = hardwareMap.get(Servo.class, "bottomservo");
-    double topservoposition = top.getPosition();
-    double bottomservoposition = down.getPosition();
-    private static double topservoincrement = 0.01;
-    private static double bottomservoincrement = 0.01;
-
 
     @Override
     public void runOpMode() {
         Servo top = hardwareMap.get(Servo.class, "topservo");
         Servo down = hardwareMap.get(Servo.class, "bottomservo");
-        top.getPosition();
+
+        double topservoposition = 0.5;  // Set to the initial position (0.5 is the mid position for Servo)
+        double bottomservoposition = 0.5;
+
+        top.setPosition(topservoposition);
+        down.setPosition(bottomservoposition);
+
+        double topservoincrement = 0.1;  // Adjust this increment based on your servo specs
+        double bottomservoincrement = 0.1;
+
         waitForStart();
+
         while (opModeIsActive()) {
-            if (gamepad1.dpad_up){
-                topservoposition -= topservoincrement;
-                top.setPosition(topservoposition);
-                telemetry.addData("Dpad", "signal worksup!");
-            }
-            if (gamepad1.dpad_down){
-                topservoposition += topservoincrement;
-                top.setPosition(topservoposition);
-                telemetry.addData("Dpad", "signal worksdown!");
-            }
-            if (gamepad1.dpad_left){
-                bottomservoposition -= bottomservoincrement;
-                top.setPosition(bottomservoposition);
-
-                telemetry.addData("Dpad", "signal worksleft!");
+            if (gamepad1.dpad_up) {
+                bottomservoposition -= 0.2;  // Decrease by 20 degrees
+                down.setPosition(bottomservoposition);
+                telemetry.addData("Dpad", "signal works up!" + bottomservoposition);
             }
 
-            if (gamepad1.dpad_right){
-                bottomservoposition += bottomservoincrement;
-                top.setPosition(bottomservoposition);
-                telemetry.addData("Dpad", "signal worksright!");
+            if (gamepad1.dpad_down) {
+                bottomservoposition += 0.2;  // Increase by 20 degrees
+                down.setPosition(bottomservoposition);
+                telemetry.addData("Dpad", "signal works down!" + bottomservoposition);
             }
+
+            if (gamepad1.dpad_left) {
+                topservoposition -= 0.2;  // Decrease by 20 degrees
+                top.setPosition(topservoposition);
+                telemetry.addData("Dpad", "signal works left!" + topservoposition);
+            }
+
+            if (gamepad1.dpad_right) {
+                topservoposition += 0.2;  // Increase by 20 degrees
+                top.setPosition(topservoposition);
+                telemetry.addData("Dpad", "signal works right!" + topservoposition);
+            }
+
             telemetry.update();
         }
     }
 }
+
