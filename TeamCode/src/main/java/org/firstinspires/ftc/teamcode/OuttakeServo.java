b1eed8dac13b0a6c19f8c5e8f9618da3ecabf05d
SynnovationFTC <145935156+SynnovationFTC@users.noreply.github.com>
2024-01-13 13:23:42 -0600

    Update OuttakeServo.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OuttakeServo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OuttakeServo.java
@@ -1,30 +1,30 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
-
+@TeleOp
 public class OuttakeServo extends LinearOpMode {
     @Override
     public void runOpMode(){
         Servo top = hardwareMap.get(Servo.class, "topservo");
+        top.setDirection(Servo.Direction.REVERSE);
         Servo bottom = hardwareMap.get(Servo.class, "bottomservo");
-        double initialtopservoposition = 0.5;
+
+        double initialtopservoposition = 0.05;
         double initialbottomservoposition = 0.5;
         top.setPosition(initialtopservoposition);
         bottom.setPosition(initialbottomservoposition);
         waitForStart();
         while (opModeIsActive()) {
-            if (gamepad1.dpad_up){
-                double currentTopUpPosition = top.getPosition();
-                double newTopUpPosition = currentTopUpPosition + 0.1;
-                top.setPosition(newTopUpPosition);
+            if (gamepad1.dpad_down){
+                top.setPosition(0.05);
                 telemetry.addData("Servo Position:", top.getPosition());
             }
-            if (gamepad1.dpad_down){
-                double currentTopDownPosition = top.getPosition();
-                double newTopDownPosition = currentTopDownPosition - 0.1;
-                top.setPosition(newTopDownPosition);
+            if (gamepad1.dpad_up){
+                top.setPosition(0.45);
                 telemetry.addData("Servo Position:", top.getPosition());
+
             }
             telemetry.update();
         }
