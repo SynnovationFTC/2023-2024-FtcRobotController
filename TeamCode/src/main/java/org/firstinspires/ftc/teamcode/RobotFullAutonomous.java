fe5b8bf661d2045ee91a16187a4a0222e5962390
SynnovationFTC <145935156+SynnovationFTC@users.noreply.github.com>
2024-02-04 17:26:41 -0600

    Update RobotFullAutonomous.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotFullAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotFullAutonomous.java
@@ -115,6 +115,7 @@ public class RobotFullAutonomous extends LinearOpMode {
         TrajectorySequence redaudienceright = drive.trajectorySequenceBuilder(new Pose2d(-37.71, -61.94, Math.toRadians(90.00)))
                 //TODO: Path needs to be remade
                 //TODO: The pixel isn't being pushed far enough onto the mark
+                //TODO: It is also running over the other team's spike mark
                 .lineTo(new Vector2d(-48.45, -61.13))
                 .lineTo(new Vector2d(-48.30, -39.25))
                 .splineTo(new Vector2d(-34.66, -34.87), Math.toRadians(0.00))
